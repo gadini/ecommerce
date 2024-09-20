2d947411a3fbbe6c5994cf9f9c4a29430720090e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-20 20:43:56 -0300

    Implements ProdutoResponseDTO toDto

--- src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
@@ -29,8 +29,12 @@ public class ProdutoController {
     @GetMapping
     public List<ProdutoResponseDto> list(){
 
-        List<Produto> produtos = produtoService.list();
-        return null;
+        List<ProdutoResponseDto> dtos = produtoService.list()
+        .stream()
+        .map(e -> new ProdutoResponseDto().toDto(e))
+        .toList();
+
+        return dtos;
     }
 
     @PostMapping
--- src/main/java/br/com/fiap/ecommerce/dtos/ProdutoResponseDto.java
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoResponseDto.java
@@ -1,9 +1,19 @@
 package br.com.fiap.ecommerce.dtos;
 
+import org.modelmapper.ModelMapper;
+
+import br.com.fiap.ecommerce.model.Produto;
+
 public class ProdutoResponseDto {
     
     private Long id;
     private String nome;
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public ProdutoResponseDto toDto(Produto produto){
+        ProdutoResponseDto result = modelMapper.map(produto, ProdutoResponseDto.class);
+        return result;
+    }
     
     public Long getId() {
         return id;
